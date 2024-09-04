f09e830976f72798dbb4da64c7440e388a2c7c59
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-09-04 19:52:49 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,73 @@
+import cv2
 import numpy as np
-
+import torch
+from NeuFlow.backbone_v7 import ConvBlock
+from NeuFlow.neuflow import NeuFlow
 
 UNKNOWN_FLOW_THRESH = 1e7
 SMALLFLOW = 0.0
 LARGEFLOW = 1e8
 
+
+def load_model(model_path, device, input_shape=(480, 640), half=False):
+    model = NeuFlow().to(device)
+
+    checkpoint = torch.load(model_path, map_location=device)
+
+    model.load_state_dict(checkpoint['model'], strict=True)
+    model = fuse_model_conv_and_bn(model)
+    model.eval()
+
+    model.init_bhwd(1, input_shape[0], input_shape[1], device, half)
+    return model
+
+
+def process_image(image_path, image_width=768, image_height=432, dtype=torch.float32):
+    image = cv2.imread(image_path)
+
+    image = cv2.resize(image, (image_width, image_height))
+
+    image = torch.from_numpy(image).permute(2, 0, 1).to(dtype) / 255.0
+    return image[None].cuda()
+
+
+def fuse_conv_and_bn(conv, bn):
+        """Fuse Conv2d() and BatchNorm2d() layers https://tehnokv.com/posts/fusing-batchnorm-and-conv/."""
+        fusedconv = (
+            torch.nn.Conv2d(
+                conv.in_channels,
+                conv.out_channels,
+                kernel_size=conv.kernel_size,
+                stride=conv.stride,
+                padding=conv.padding,
+                dilation=conv.dilation,
+                groups=conv.groups,
+                bias=True,
+            )
+            .requires_grad_(False)
+            .to(conv.weight.device)
+        )
+
+        # Prepare filters
+        w_conv = conv.weight.clone().view(conv.out_channels, -1)
+        w_bn = torch.diag(bn.weight.div(torch.sqrt(bn.eps + bn.running_var)))
+        fusedconv.weight.copy_(torch.mm(w_bn, w_conv).view(fusedconv.weight.shape))
+
+        # Prepare spatial bias
+        b_conv = torch.zeros(conv.weight.shape[0], device=conv.weight.device) if conv.bias is None else conv.bias
+        b_bn = bn.bias - bn.weight.mul(bn.running_mean).div(torch.sqrt(bn.running_var + bn.eps))
+        fusedconv.bias.copy_(torch.mm(w_bn, b_conv.reshape(-1, 1)).reshape(-1) + b_bn)
+
+        return fusedconv
+
+def fuse_model_conv_and_bn(model):
+    for m in model.modules():
+        if type(m) is ConvBlock:
+            m.conv1 = fuse_conv_and_bn(m.conv1, m.norm1)  # update conv
+            m.conv2 = fuse_conv_and_bn(m.conv2, m.norm2)  # update conv
+            delattr(m, "norm1")  # remove batchnorm
+            delattr(m, "norm2")  # remove batchnorm
+            m.forward = m.forward_fuse  # update forward
 
 def make_color_wheel():
     """
